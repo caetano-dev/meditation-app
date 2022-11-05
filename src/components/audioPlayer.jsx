f12c4f8299e720de432662a5fd15b81c61c99b7f
Gabriel Pereira <gabrielpmonte@hotmail.com>
2022-11-05 17:49:21 -0300

    Fix sound file extension mp4 to m4a

--- src/components/audioPlayer.jsx
+++ src/components/audioPlayer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactAudioPlayer from 'react-audio-player';
 
-function AudioPlayer({name, description, style}) {
+function AudioPlayer({ name, description, style }) {
   return (
     <div className={style}>
       <h2>{name}</h2>
       <h3>{description}</h3>
-      <ReactAudioPlayer src={`../../sounds/${name}.mp4`} controls />
+      <ReactAudioPlayer src={`../../sounds/${name}.m4a`} controls />
     </div>
   );
 }
